87aaaa3f675fe6a746d632cb64d733e13328d9e6
Yujilong <2069388340@qq.com>
2020-07-22 15:03:05 +0800

    start

---
+++ Assets/MiddleGround/MG_Scripts/MG_ADS/AdjustEventLogger.cs
@@ -0,0 +1,126 @@
+﻿using com.adjust.sdk;
+using MiddleGround;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+#if UNITY_IOS && !UNITY_EDITOR
+using System.Runtime.InteropServices;
+#endif
+
+public class AdjustEventLogger : MonoBehaviour
+{
+#if UNITY_IOS && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern string Getidfa();
+#endif
+#if UNITY_IOS
+    public const string APP_TOKEN = "mga1qo7h0ge8";
+    public const string TOKEN_open = "9rpph2";
+    public const string TOKEN_ad = "qq0bje";
+    public const string TOKEN_noads = "i2981u";
+    public const string TOKEN_stage_end = "hoi51j";
+    public const string TOKEN_wheel="77ya28";
+    public const string TOKEN_slots="3jwo2v";
+    public const string TOKEN_ggl = "li80rh";
+    public const string TOKEN_deeplink = "hhrdvl";
+    public const string TOKEN_packb = "acha2z";
+#elif UNITY_ANDROID
+    public const string APP_TOKEN = "jd7c6pgj74lc";
+    public const string TOKEN_open = "uhq3e9";
+    public const string TOKEN_ad = "dq77bh";
+    public const string TOKEN_noads = "kzt12m";
+    /// <summary>
+    /// dice
+    /// </summary>
+    public const string TOKEN_stage_end = "f8d9fb";
+    public const string TOKEN_wheel = "iscrs0";
+    public const string TOKEN_slots = "cmcx0h";
+    /// <summary>
+    /// scratch
+    /// </summary>
+    public const string TOKEN_ggl = "8cn8pq";
+    public const string TOKEN_deeplink = "6sm9ed";
+    public const string TOKEN_packb = "nij82z";
+#endif
+    public static AdjustEventLogger Instance;
+    private void Awake()
+    {
+        Instance = this;
+#if UNITY_EDITOR
+        AdjustConfig adjustConfig = new AdjustConfig(APP_TOKEN, AdjustEnvironment.Sandbox);
+#else
+        AdjustConfig adjustConfig = new AdjustConfig(APP_TOKEN, AdjustEnvironment.Production);
+#endif
+        adjustConfig.sendInBackground = true;
+        adjustConfig.launchDeferredDeeplink = true;
+        adjustConfig.eventBufferingEnabled = true;
+        adjustConfig.logLevel = AdjustLogLevel.Info;
+        adjustConfig.setAttributionChangedDelegate(OnAttributionChangedCallback);
+        Adjust.start(adjustConfig);
+    }
+    private void Start()
+    {
+        GetAdID();
+        StartCoroutine(CheckAttributeTo());
+        MG_Manager.Instance.SendAdjustGameStartEvent();
+    }
+    public void AdjustEventNoParam(string token)
+    {
+        AdjustEvent adjustEvent = new AdjustEvent(token);
+        Adjust.trackEvent(adjustEvent);
+    }
+    public void AdjustEvent(string token, params (string key, string value)[] list)
+    {
+        AdjustEvent adjustEvent = new AdjustEvent(token);
+        foreach (var (key, value) in list)
+        {
+            adjustEvent.addCallbackParameter(key, value);
+        }
+        Adjust.trackEvent(adjustEvent);
+    }
+    void OnAttributionChangedCallback(AdjustAttribution attribution)
+    {
+        if (attribution.network.Equals("Organic"))
+        {
+        }
+        else
+        {
+            if (!MG_Manager.Instance.loadEnd)
+                MG_Manager.Instance.Set_Save_isPackB();
+        }
+    }
+    private string AppName = Ads.AppName;
+    private string ifa;
+    private IEnumerator CheckAttributeTo()
+    {
+        yield return new WaitUntil(() => !string.IsNullOrEmpty(ifa));
+        string url = string.Format("http://a.mobile-mafia.com:3838/adjust/is_unity_clicked?ifa={0}&app_name={1}", ifa, AppName);
+
+        var web = new UnityWebRequest(url);
+        web.downloadHandler = new DownloadHandlerBuffer();
+        yield return web.SendWebRequest();
+        if (web.responseCode == 200)
+        {
+            if (web.downloadHandler.text.Equals("1"))
+            {
+                if (!MG_Manager.Instance.loadEnd)
+                    MG_Manager.Instance.Set_Save_isPackB();
+            }
+        }
+    }
+    private void GetAdID()
+    {
+#if UNITY_ANDROID
+        Application.RequestAdvertisingIdentifierAsync(
+           (string advertisingId, bool trackingEnabled, string error) =>
+           {
+               ifa = advertisingId;
+           }
+       );
+#elif UNITY_IOS && !UNITY_EDITOR
+        ifa = Getidfa();
+#endif
+    }
+}
+
---
+++ Assets/MiddleGround/MG_Scripts/MG_ADS/Ads.cs
@@ -0,0 +1,156 @@
+﻿using MiddleGround;
+using MiddleGround.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ads : MonoBehaviour
+{
+#if UNITY_ANDROID
+	private const string APP_KEY = "cc25bf45";
+#elif UNITY_IOS
+	private const string APP_KEY = "cc25f8cd";
+#endif
+	public static Ads _instance;
+	public string adDes = string.Empty;
+	public const string AppName = "A017_midlott";
+	private void Awake()
+	{
+		_instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start()
+	{
+		//Dynamic config example
+		IronSourceConfig.Instance.setClientSideCallbacks(true);
+
+		string id = IronSource.Agent.getAdvertiserId();
+
+		//IronSource.Agent.validateIntegration();
+
+		// SDK init
+		IronSource.Agent.init(APP_KEY);
+		IronSource.Agent.loadInterstitial();
+
+	}
+	public bool ShowRewardVideo(Action rewardedCallback, int clickAdTime,string des)
+	{
+		adDes = des;
+		rewardCallback = rewardedCallback;
+#if UNITY_EDITOR
+		rewardedCallback();
+		return true;
+#endif
+#if UNITY_IOS
+		if (!MG_Manager.Instance.Get_Save_PackB())
+		{
+			rewardCallback();
+			return true;
+		}
+#endif
+		if (IronSource.Agent.isRewardedVideoAvailable())
+		{
+			IronSource.Agent.showRewardedVideo();
+			return true;
+		}
+		else
+		{
+			StartCoroutine(WaitLoadAD(true,clickAdTime));
+			return false;
+		}
+	}
+	float interstialLasttime = 0;
+	public void ShowInterstialAd(Action callback, string des)
+	{
+		popCallback = callback;
+#if UNITY_EDITOR
+		callback();
+		return;
+#endif
+		adDes = des;
+#if UNITY_IOS
+		if (!MG_Manager.Instance.Get_Save_PackB()) 
+		{
+			callback();
+			return;
+		}
+#endif
+		if (Time.realtimeSinceStartup - interstialLasttime < 30)
+        {
+			callback();
+			return;
+        }
+		if (IronSource.Agent.isInterstitialReady())
+		{
+			interstialLasttime = Time.realtimeSinceStartup;
+			IronSource.Agent.showInterstitial();
+		}
+		else
+		{
+			callback();
+			MG_Manager.Instance.SendAdjustPlayAdEvent(false, false, adDes);
+		}
+	}
+	void OnApplicationPause(bool isPaused)
+	{
+		IronSource.Agent.onApplicationPause(isPaused);
+	}
+	public GameObject notice;
+	const string text = "No Video is ready , please try again later.";
+	public void ShowNoAdNotice()
+	{
+		MG_Manager.Instance.Show_PopTipsPanel(text);
+	}
+	IEnumerator WaitLoadAD(bool isRewardedAd,int clickAdTime)
+	{
+		notice.SetActive(true);
+		StringBuilder content = new StringBuilder("Loading.");
+		Text noticeText = notice.GetComponentInChildren<Text>();
+		noticeText.text = content.ToString();
+		int timeOut = 6;
+		while (timeOut > 0)
+		{
+			yield return new WaitForSeconds(Time.timeScale);
+			timeOut--;
+			content.Append('.');
+			noticeText.text = content.ToString();
+			if (isRewardedAd && IronSource.Agent.isRewardedVideoAvailable())
+			{
+				IronSource.Agent.showRewardedVideo();
+				notice.SetActive(false);
+				yield break;
+			}
+		}
+		MG_Manager.Instance.SendAdjustPlayAdEvent(false, true, adDes);
+		if (clickAdTime >= 2)
+		{
+			MG_UIManager.Instance.CloseTopPopPanelAsync();
+			noticeText.text = text;
+			yield return new WaitForSeconds(2 * Time.timeScale);
+		}
+		notice.SetActive(false);
+	}
+	Action rewardCallback;
+	private bool canGetReward = false;
+	public void GetReward()
+	{
+		canGetReward = true;
+	}
+	public void InvokeGetRewardMethod()
+	{
+		if (canGetReward)
+		{
+			rewardCallback();
+			canGetReward = false;
+		}
+	}
+	Action popCallback;
+	public void InvokePopAd()
+    {
+		popCallback();
+    }
+}
